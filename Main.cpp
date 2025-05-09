f5613c2cc419fe1ec408bc4b70670b9070bddf70
FOLINK <108606294+IDMoquan@users.noreply.github.com>
2025-05-09 10:12:50 +0800

    Code : Read .pcap files

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
+#include<fstream>
 #include<queue>
 #include "Head.h"
+#define Pcap_File "./"
 
 using namespace std;
 
 
-
 int main(){
-    Pcap_Header ph;
-    Pcap_Packet_Header pph;
+    Pcap_Header *ph;
+    Pcap_Packet_Header *pph;
 
+    ifstream pf;
+    pf.open(Pcap_File, ios::in | ios::binary);
+    if(!pf){
+        cout << "Open File Error!" << endl;
+        return -1;
+    }
+    pf.read(ph,sizeof(Pcap_Header));
+    
     return 0;
 }
